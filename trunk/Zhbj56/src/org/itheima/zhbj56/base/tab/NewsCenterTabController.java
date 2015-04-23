e0c5c4d8cbd099db4c0a12d0ea846cf6a9761d64
xq <xq@6d9ff394-bb5e-5748-becc-c6ec5218e479>
2015-04-23 02:34:51 +0000

    [code]http请求的访问
    git-svn-id: https://desktop-feedbts/svn/Zhbj56@25 6d9ff394-bb5e-5748-becc-c6ec5218e479

--- trunk/Zhbj56/src/org/itheima/zhbj56/base/tab/NewsCenterTabController.java
+++ trunk/Zhbj56/src/org/itheima/zhbj56/base/tab/NewsCenterTabController.java
@@ -1,9 +1,18 @@
 package org.itheima.zhbj56.base.tab;
 
 import org.itheima.zhbj56.base.TabController;
+import org.itheima.zhbj56.utils.Constans;
+
+import com.lidroid.xutils.HttpUtils;
+import com.lidroid.xutils.exception.HttpException;
+import com.lidroid.xutils.http.RequestParams;
+import com.lidroid.xutils.http.ResponseInfo;
+import com.lidroid.xutils.http.callback.RequestCallBack;
+import com.lidroid.xutils.http.client.HttpRequest.HttpMethod;
 
 import android.content.Context;
 import android.graphics.Color;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.TextView;
@@ -25,7 +34,8 @@ import android.widget.TextView;
 public class NewsCenterTabController extends TabController
 {
 
-	private TextView	tv;
+	protected static final String	TAG	= null;
+	private TextView				tv;
 
 	public NewsCenterTabController(Context context) {
 		super(context);
@@ -53,5 +63,45 @@ public class NewsCenterTabController extends TabController
 
 		// 设置内容数据
 		tv.setText("新闻中心的内容");
+
+		// 去网络加载数据
+		// Url,请求方式,请求参数，消息头
+		HttpUtils utils = new HttpUtils();
+
+		// 异步的网络访问
+		// 请求方式:
+		// Url:
+		// 请求参数，消息头
+		String url = Constans.NEWSCENTER_URL;
+		// RequestParams params = new RequestParams();
+		// params.addBodyParameter(name, value);//请求内容中的
+		// params.addQueryStringParameter(name, value);//请求行中的
+		// params.addHeader(name, value);// 请求消息头
+
+		utils.send(HttpMethod.GET, url, new RequestCallBack<String>() {
+
+			@Override
+			public void onSuccess(ResponseInfo<String> responseInfo)
+			{
+				// int statusCode = responseInfo.statusCode;//状态码
+				// 访问服务器成功
+				// 访问接口是否成功
+
+				// 状态码,内容
+				String result = responseInfo.result;
+
+				Log.d(TAG, "访问接口成功:" + result);
+
+			}
+
+			@Override
+			public void onFailure(HttpException error, String msg)
+			{
+				// 访问服务器不成功
+				Log.d(TAG, "访问服务器失败:" + msg);
+
+			}
+		});
+
 	}
 }
